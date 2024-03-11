756a6740e3fd88a65c80c901d28f0c311bdc28fc
Paveel2004 <148257086+Paveel2004@users.noreply.github.com>
2024-03-11 18:19:58 +0300

    Добавлен мониторинг использования диска

---
+++ Class/Dynamic data/UsageDisk.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalClass.Dynamic_data
+{
+    public class UsageDisk
+    {
+        public string SerialNumberBIOS { get; set; }
+        public double FreeSpace { get; set; }
+        public DateTime DateTime {get;set;}
+        public UsageDisk(double FreeSpace, string SerialNumberBIOS) {
+            this.SerialNumberBIOS = SerialNumberBIOS;
+            this.FreeSpace = FreeSpace;
+            DateTime = DateTime.Now;
+
+        }
+
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -31,7 +31,37 @@ namespace Server
             Task.Run(() => StartServer(9580, HendleClientUsageCPU));
             Task.Run(() => StartServer(9510, HendleClientUsageEthernet));
             Task.Run(() => StartServer(9370, HendleClientVideoCard));
+            Task.Run(() => StartServer(9300, HendleClientUsageDisk));
             Console.ReadLine();
+        }
+        static void HendleClientUsageDisk(TcpClient tcpClient)
+        {
+            try
+            {
+                using NetworkStream stream = tcpClient.GetStream();
+
+                byte[] data = new byte[5000];
+                int bytesRead;
+
+                // Читаем данные из потока
+                while ((bytesRead = stream.Read(data, 0, data.Length)) != 0)
+                {
+                    string message = Encoding.UTF8.GetString(data, 0, bytesRead);
+                    var usageDisk = JsonConvert.DeserializeObject<UsageDisk>(message);
+                    DataBaseHelper.Query($"EXECUTE ДобавитьИспользование @ТипХарактеристики = 'Диск', @Характеристика = 'Свободное место', @СерийныйНомерBIOS = '{usageDisk.SerialNumberBIOS}', @Значение = '{usageDisk.FreeSpace}', @ДатаВремя = '{usageDisk.DateTime}'");
+                    byte[] response = Encoding.UTF8.GetBytes("Сообщение получено");
+                    stream.Write(response, 0, response.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // Закрываем соединение при завершении работы с клиентом
+                tcpClient.Close();
+            }
         }
         static void HendleClientVideoCard(TcpClient tcpClient)
         {
