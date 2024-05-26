44bcd915904fe26efb388bdbb7bcbbc8cd738603
Paveel2004 <148257086+Paveel2004@users.noreply.github.com>
2024-05-26 04:48:30 +0300

    Добавлен клас PowerShell

---
+++ AdminInterfase/CenterConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace AdminInterfase
+{
+    public class CenterConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double parentWidth = (double)value;
+            return parentWidth / 2 - 100; // Возвращаем половину ширины родительского элемента
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
