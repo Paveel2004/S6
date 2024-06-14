0cf1d832ad61ef1fe185a53a93128a94f0491e56
Paveel2004 <148257086+Paveel2004@users.noreply.github.com>
2024-06-14 17:16:33 +0300

    +Рамка + Шрифт

--- AdminInterfase/MemoryToColorConverter.cs
+++ AdminInterfase/MemoryToColorConverter.cs
@@ -10,16 +10,17 @@ namespace AdminInterfase
         {
             double memory = (double)value;
             if (memory < 50)
-                return Brushes.Green;
+                return new SolidColorBrush(Color.FromRgb(0, 100, 0)); // DarkGreen
             else if (memory >= 60 && memory < 70)
-                return Brushes.Yellow;
+                return new SolidColorBrush(Color.FromRgb(139, 139, 0)); // DarkYellow
             else if (memory >= 70 && memory < 90)
-                return Brushes.Orange;
+                return new SolidColorBrush(Color.FromRgb(255, 140, 0)); // DarkOrange
             else if (memory >= 90)
-                return Brushes.Red;
+                return new SolidColorBrush(Color.FromRgb(139, 0, 0)); // DarkRed
 
             return Brushes.Black;
         }
+
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
